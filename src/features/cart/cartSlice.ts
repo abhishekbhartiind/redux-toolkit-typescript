b309bb0052b962fa6580f29aa3bd7f4d336f6749
abhishekbhartiind <imabhishekbharti12@gmail.com>
2022-01-05 02:33:11 +0530

    react, typescript, redux toolkit features

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,128 @@
+import {
+  createSlice,
+  PayloadAction,
+  createSelector,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+import type { RootState, AppDispatch } from "../../app/store";
+import { checkout, CartItems } from "../../app/api";
+
+type CheckoutState = "LOADING" | "READY" | "ERROR";
+export interface CartState {
+  items: {
+    [productId: string]: number;
+  };
+  checkoutState: CheckoutState;
+  errorMessage: string;
+}
+
+const initialState: CartState = {
+  items: {},
+  checkoutState: "READY",
+  errorMessage: "",
+};
+
+export const checkoutCart = createAsyncThunk<
+  { success: boolean },
+  undefined,
+  { state: RootState }
+>("cart/checkout", async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const items = state.cart.items;
+  const response = await checkout(items);
+  return response;
+});
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      if (state.items[id]) {
+        state.items[id]++;
+      } else {
+        state.items[id] = 1;
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      delete state.items[action.payload];
+    },
+    updateQuantity(
+      state,
+      action: PayloadAction<{ id: string; quantity: number }>
+    ) {
+      const { quantity, id } = action.payload;
+      state.items[id] = quantity;
+    },
+  },
+  extraReducers: function (builder) {
+    builder.addCase(checkoutCart.pending, (state, action) => {
+      state.checkoutState = "LOADING";
+    });
+    builder.addCase(
+      checkoutCart.fulfilled,
+      (state, action: PayloadAction<{ success: boolean }>) => {
+        const { success } = action.payload;
+        if (success) {
+          state.checkoutState = "READY";
+          state.items = {};
+        } else {
+          state.checkoutState = "ERROR";
+        }
+      }
+    );
+    builder.addCase(checkoutCart.rejected, (state, action) => {
+      state.checkoutState = "ERROR";
+      state.errorMessage = action.error.message || "";
+    });
+    // builder.addCase("cart/checkout/pending", (state, action) => {
+    //   state.checkoutState = "LOADING";
+    // });
+    // builder.addCase("cart/checkout/fulfilled", (state, action) => {
+    //   state.checkoutState = "READY";
+    // });
+  },
+});
+
+// export function checkout() {
+//   return function checkoutThunk(dispatch: AppDispatch) {
+//     dispatch({
+//       type: "cart/checkout/pending",
+//     });
+//     setTimeout(() => dispatch({ type: "cart/checkout/fulfilled" }), 500);
+//   };
+// }
+
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
+export function getNumItems(state: RootState) {
+  let numItems = 0;
+  for (let id in state.cart.items) {
+    numItems += state.cart.items[id];
+  }
+  return numItems;
+}
+
+export const getMemoizedNumItems = createSelector(
+  (state: RootState) => state.cart.items,
+  (items) => {
+    let numItems = 0;
+    for (let id in items) {
+      numItems += items[id];
+    }
+    return numItems;
+  }
+);
+
+export const getTotalPrice = createSelector(
+  (state: RootState) => state.cart.items,
+  (state: RootState) => state.products.products,
+  (items, products) => {
+    let total = 0;
+    for (let id in items) {
+      total += products[id].price * items[id];
+    }
+    return total.toFixed(2);
+  }
+);
---
+++ src/features/products/productSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { Product } from "../../app/api";
+
+export interface ProductState {
+  products: {
+    [id: string]: Product;
+  };
+}
+
+const initialState: ProductState = {
+  products: {},
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    receivedProducts(state, action: PayloadAction<Product[]>) {
+      const products = action.payload;
+      products.forEach((product) => {
+        state.products[product.id] = product;
+      });
+    },
+  },
+});
+
+export const { receivedProducts } = productSlice.actions;
+export default productSlice.reducer;
